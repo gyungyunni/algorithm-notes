b89f64bd2d6a9ee54f886376573575a02b3e9194
mingyun <mingyun8646@nate.com>
2023-12-29 18:20:52 +0900

    feat:  HashTable, HashMap

---
+++ algorithm-notes/src/main/java/dataStructure/linear/h/HashMapEx.java
@@ -0,0 +1,30 @@
+package dataStructure.linear.h;
+
+import java.util.HashMap;
+
+// 해시 테이블 보다 빠르고, null 키를 허용함
+// 동기화되지 않아서 스레드로부터 안전하지 않음
+public class HashMapEx {
+    public static void main(String[] args) {
+        // create hash map
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        // insert data
+        hashMap.put("apple", 5);
+        hashMap.put("banana", 7);
+        hashMap.put("cherry", 3);
+
+        // data query
+        System.out.println("apple: " + hashMap.get("apple"));
+        System.out.println("banana: " + hashMap.get("banana"));
+        System.out.println("grape: " + hashMap.get("grape"));
+
+        // delete data
+        hashMap.remove("apple");
+
+        // HashMap output
+        for (var entry : hashMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
+}
--- algorithm-notes/src/main/java/dataStructure/linear/h/HashTable.java
+++ algorithm-notes/src/main/java/dataStructure/linear/h/HashTable.java
@@ -1,4 +1,36 @@
 package dataStructure.linear.h;
 
+import java.util.Hashtable;
+import java.util.Map;
+
+// 장점: 빠른 검색, 동적 크기 조정, null 허용 안함, hashmap보다 느림
+// 동기화되아서 스레드로부터 안전함
 public class HashTable {
+    public static void main(String[] args) {
+        // 해시 테이블 생성
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+
+        // 데이터 삽입
+        hashtable.put("apple", 5);
+        hashtable.put("banana", 7);
+        hashtable.put("cherry", 3);
+
+        // 데이터 조회
+        System.out.println("apple: " + hashtable.get("apple"));
+        System.out.println("banana: " + hashtable.get("banana"));
+        System.out.println("grape: " + hashtable.get("grape"));
+
+        // 데이터 삭제
+        hashtable.remove("apple");
+
+//        // 해시 테이블 출력
+//        for (Map.Entry<String, Integer> entry : hashtable.entrySet()) {
+//            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+//        }
+
+        for (var entry : hashtable.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+    }
 }
