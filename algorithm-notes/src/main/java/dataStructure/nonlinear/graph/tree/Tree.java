45cd18dbe8cf104a3c62402e69e6bd4081475de9
mingyun <mingyun8646@nate.com>
2024-01-01 22:51:38 +0900

    feat: Tree

--- algorithm-notes/src/main/java/dataStructure/nonlinear/graph/tree/Tree.java
+++ algorithm-notes/src/main/java/dataStructure/nonlinear/graph/tree/Tree.java
@@ -1,4 +1,60 @@
 package dataStructure.nonlinear.graph.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    String value;
+    List<TreeNode> children;
+
+    public TreeNode(String value) {
+        this.value = value;
+        this.children = new ArrayList<>();
+    }
+
+    public void addChild(TreeNode childNode) {
+        this.children.add(childNode);
+    }
+
+    @Override
+    public String toString() {
+        return toString(this, 0);
+    }
+
+    private String toString(TreeNode node, int level) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            stringBuilder.append("\t");
+        }
+        stringBuilder.append(node.value).append("\n");
+        for (TreeNode child : node.children) {
+            stringBuilder.append(toString(child, level + 1));
+        }
+        return stringBuilder.toString();
+    }
+}
+
 public class Tree {
+    public static void main(String[] args) {
+        // 트리 생성
+        TreeNode root = new TreeNode("Root");
+
+        // 노드 추가
+        TreeNode child1 = new TreeNode("Child 1");
+        TreeNode child2 = new TreeNode("Child 2");
+        TreeNode child3 = new TreeNode("Child 3");
+
+        root.addChild(child1);
+        root.addChild(child2);
+        root.addChild(child3);
+
+        TreeNode child11 = new TreeNode("Child 1.1");
+        TreeNode child12 = new TreeNode("Child 1.2");
+
+        child1.addChild(child11);
+        child1.addChild(child12);
+
+        // 트리 출력
+        System.out.println(root);
+    }
 }
--- algorithm-notes/src/main/java/dataStructure/nonlinear/graph/tree/trie/Trie.java
+++ algorithm-notes/src/main/java/dataStructure/nonlinear/graph/tree/trie/Trie.java
@@ -1,4 +1,58 @@
 package dataStructure.nonlinear.graph.tree.trie;
 
-public class Trie {
+import java.util.HashMap;
+import java.util.Map;
+
+class TrieNode {
+    Map<Character, TrieNode> children;
+    boolean isEndOfWord;
+
+    public TrieNode() {
+        this.children = new HashMap<>();
+        this.isEndOfWord = false;
+    }
+}
+
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            node.children.putIfAbsent(ch, new TrieNode());
+            node = node.children.get(ch);
+        }
+        node.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (!node.children.containsKey(ch)) {
+                return false;
+            }
+            node = node.children.get(ch);
+        }
+        return node.isEndOfWord;
+    }
+
+    public static void main(String[] args) {
+        // Trie 생성
+        Trie myTrie = new Trie();
+
+        // 단어 삽입
+        myTrie.insert("apple");
+        myTrie.insert("app");
+        myTrie.insert("banana");
+
+        // 단어 검색
+        System.out.println("Search 'apple': " + myTrie.search("apple"));
+        System.out.println("Search 'app': " + myTrie.search("app"));
+        System.out.println("Search 'banana': " + myTrie.search("banana"));
+        System.out.println("Search 'grape': " + myTrie.search("grape"));
+    }
 }
