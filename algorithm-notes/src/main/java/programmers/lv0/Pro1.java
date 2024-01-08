69bbd5e032e5f1c342142e19c3ab82508199e237
mingyun <mingyun8646@nate.com>
2024-01-08 16:48:01 +0900

    feat: 프로그래머스 1번

--- algorithm-notes/src/main/java/programmers/lv0/Pro1.java
+++ algorithm-notes/src/main/java/programmers/lv0/Pro1.java
@@ -2,9 +2,7 @@ package programmers.lv0;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/250133
 
-import java.util.Scanner;
-
-public class Programmers1 {
+public class Pro1 {
     public static void main(String[] args) {
         String msg = "Spring is beginning";
         int val1 = 3;
