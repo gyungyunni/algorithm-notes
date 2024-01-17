553ba9e69d161588af23f86a1ecd739cedcf73dc
mingyun <mingyun8646@nate.com>
2024-01-17 19:29:47 +0900

    feat: 해시맵

--- algorithm-notes/src/main/java/dataStructure/linear/h/HashMapEx.java
+++ algorithm-notes/src/main/java/dataStructure/linear/h/HashMapEx.java
@@ -3,6 +3,7 @@ package dataStructure.linear.h;
 import java.util.HashMap;
 
 // 해시 테이블 보다 빠르고, null 키를 허용함
+// key 값은 중복을 허용하지 않음. 값은 동일해도 됨
 // 동기화되지 않아서 스레드로부터 안전하지 않음
 public class HashMapEx {
     public static void main(String[] args) {
