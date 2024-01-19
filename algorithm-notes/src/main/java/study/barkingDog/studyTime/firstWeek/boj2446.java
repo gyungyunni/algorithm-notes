26adcd33bf54e8e34fe0c6598d73a634b834f859
mingyun <mingyun8646@nate.com>
2024-01-19 22:27:39 +0900

    feat: 알고리즘 스터디 첫주

---
+++ algorithm-notes/src/main/java/study/barkingDog/studyTime/firstWeek/boj2446.java
@@ -0,0 +1,20 @@
+package study.barkingDog.studyTime.firstWeek;
+
+import java.util.*;
+
+public class boj2446 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(" ".repeat(i));
+            System.out.println("*".repeat(2 * (n - i) - 1));
+        }
+
+        for (int i = 1; i < n; i++) {
+            System.out.print(" ".repeat(n - 1 - i));
+            System.out.println("*".repeat(2 * i + 1));
+        }
+    }
+}
